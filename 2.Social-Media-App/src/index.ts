0ffe26e08dc3e48d35b1cd398724a952438661d2
armando.ferrara <armando.ferrara@dormakaba.com>
2022-07-12 11:10:15 +0200

    Data Relationships added

--- 2.Social-Media-App/src/index.ts
+++ 2.Social-Media-App/src/index.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema';
-import { Query, Mutation } from './resolvers';
+import { Query, Mutation, Profile, Post, User } from './resolvers';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { getUserFromToken } from './utils/getUserFromToken';
 
@@ -21,6 +21,9 @@ const server = new ApolloServer({
 	typeDefs,
 	resolvers: {
 		Query,
+		Profile,
+		Post,
+		User,
 		Mutation,
 	},
 	context: async ({ req }: any): Promise<Context> => {
